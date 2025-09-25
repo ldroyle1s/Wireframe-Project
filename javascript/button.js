77c412d2fb5719c78517ae28d2d3ba42347032a6
LDRoyle1s <LDRoyle1s@capetigers.com>
2025-09-25 10:44:06 -0500

    second Commit

---
+++ javascript/button.js
@@ -0,0 +1,8 @@
+ function ButtonFunc() {
+            var x = document.getElementById("buttonid");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+                }
+            }
